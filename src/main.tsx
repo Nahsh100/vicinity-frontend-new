a0c59a9b4fbcaaf431f8b20398f26e817d55407b
Nahshon <nahshonkampamba10@gmail.com>
2025-12-15 16:24:40 +0200

    changed main

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { ClerkProvider } from '@clerk/clerk-react'
-import { ErrorBoundary } from './components/common/ErrorBoundary'
+import { ErrorBoundary } from './components/common/ErrorBoundary.tsx'
 import App from './App.tsx'
 import './index.css'
 
